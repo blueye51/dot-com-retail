86aaa3eaa22587d88af5ebbb49668fb1990e121a
Eric Rand <eric.rand66@gmail.com>
2025-09-25 23:49:01 +0300

    initilized vite+rect frontend, added redis for future refresh tokens, dev configs, removed after prod, simple refresh token service

---
+++ backend/store/src/main/java/com/eric/store/dto/UserLogin.java
@@ -0,0 +1,6 @@
+package com.eric.store.dto;
+
+public record UserLogin(
+    String email,
+    String password
+) {}
--- backend/store/src/main/java/com/eric/store/dto/UserRegister.java
+++ backend/store/src/main/java/com/eric/store/dto/UserRegister.java
@@ -1,9 +1,15 @@
 package com.eric.store.dto;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record UserRegister(
         @Email String email,
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\w\\s]).{8,}$",
+                message = "Password must be at least 8 characters long and include uppercase, lowercase, number, and symbol"
+        )
         String password,
-        String name
+        @NotBlank String name
 ) {}
