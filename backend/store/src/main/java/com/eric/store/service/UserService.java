0ee0360e4ff23238dfbdaff5ec6607bc750c3530
Eric Rand <eric.rand66@gmail.com>
2025-09-26 18:12:59 +0300

    implemented jwt builders, checkers, the base that is needed

--- backend/store/src/main/java/com/eric/store/service/UserService.java
+++ backend/store/src/main/java/com/eric/store/service/UserService.java
@@ -1,9 +1,19 @@
 package com.eric.store.service;
 
+import com.eric.store.entity.User;
+import com.eric.store.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User findById(UUID id) {
+        return userRepository.findById(id).orElseThrow( () -> new RuntimeException("User not found") );
+    }
 }
