74f3f58495315a6d7f53b02b5deddd97c40d17e0
Eric Rand <eric.rand66@gmail.com>
2025-09-27 21:35:44 +0300

    added some frontend, succesful fetch

---
+++ frontend/store/src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+function Header () {
+    return (
+        <header>Header Component</header>
+    )
+}
+
+export default Header
---
+++ frontend/store/src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css'
+
+function Home () {
+  return (
+    <div>
+      <h1>Welcome to the Store</h1>
+      <p>Browse our products and enjoy shopping!</p>
+    </div>
+  )
+}
+
+export default Home
